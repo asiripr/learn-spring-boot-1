0776cfa41c523448d0f433688015f8c0d6d2a18e
asiripr <asiripramodaya2000@gmail.com>
2024-08-21 05:54:44 +0530

    lombok getters and setters

--- src/main/java/com/example/Test02/model/Student.java
+++ src/main/java/com/example/Test02/model/Student.java
@@ -1,31 +1,14 @@
 package com.example.Test02.model;
 
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-@Component
+@Data
 public class Student {
 	private String id;
 	private String name;
 	private String email;
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	
 	
 }
