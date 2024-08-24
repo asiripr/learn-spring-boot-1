de5cfec1663c1f9184d3aa008f5de58935f7a614
asiripr <asiripramodaya2000@gmail.com>
2024-08-24 08:16:21 +0530

    src/main/resources/application.properties -> modified

--- src/main/java/com/example/Test02/controller/MyController.java
+++ src/main/java/com/example/Test02/controller/MyController.java
@@ -3,6 +3,7 @@ package com.example.Test02.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,11 @@ public class MyController {
 	@PutMapping("/updateStudent")
 	public String updateStudent(@RequestBody Student student) {
 		return studentService.updateStudent(student);
+	}
+	
+	@DeleteMapping("/deleteMapping/{id}")
+	public String deleteStudent(@PathVariable String id) {
+		return studentService.deleteStudent(id);
 	}
 	@GetMapping("/hi")
 	public String name() {
