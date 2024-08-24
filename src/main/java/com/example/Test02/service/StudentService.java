3d00afffdbc2a502ad659228fb49bc531711562a
asiripr <asiripramodaya2000@gmail.com>
2024-08-24 08:33:13 +0530

    deleted the manually created student objects

--- src/main/java/com/example/Test02/service/StudentService.java
+++ src/main/java/com/example/Test02/service/StudentService.java
@@ -15,24 +15,7 @@ public class StudentService {
 	List<Student> studentList = new ArrayList<Student>();
 	
 	public StudentService() {
-		Student st1 = new Student();
-		st1.setName("Sithum");
-		st1.setId("001");
-		st1.setEmail("sithum@gmail.com");
 		
-		Student st2 = new Student();
-		st2.setName("Harini");
-		st2.setId("002");
-		st2.setEmail("harini@gmail.com");
-		
-		Student st3 = new Student();
-		st3.setName("Janani");
-		st3.setId("003");
-		st3.setEmail("janani@gmail.com");
-		
-		studentList.add(st1);
-		studentList.add(st2);
-		studentList.add(st3);
 	}
 	
 	public List<Student> getAllStudents(){
